c000faa6efbfbdc5541cc42f6b43dda187caf9ce
jq2 <pr1v7bot@gmail.com>
2019-02-16 03:06:58 -0200

    Update src/bot.py     * Added some tests     * Added basic structured flow

--- src/bot.py
+++ src/bot.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python
 import os
+import json
 
 
-update_id = None
 isDebugOn = True
 BOT_TOKEN = None
 
 
+class UpdateMessage:
+
+    def __init__(self):
+        self.update_id = None
+
+    def getUpdates(self, limit, offset):
+        # curl to api and return a list of json objects
+        # self.update_id = '{"key" : "value"}'
+        # self.update_id = dict()
+        # self.update_id = 999999
+        # self.update_id = None
+        self.update_id = "Hello, world!, new message received!!!"
+        return self.update_id
+
+
+
 class telegram:
+
+    update_id = None
 
     def __init__(self, token):
         self.__token = token
         self.__update_id = -1
         self.__updates = []
+        self.update_id = 0
 
     def set_token(self, token):
         if isDebugOn:
@@ -20,7 +39,13 @@ class telegram:
 
     def get_updates(self):
         if isDebugOn:
-            print("[DEBUG] call get_updates")
+            print("[DEBUG:22222] call get_updates")
+
+        new_updates = UpdateMessage()
+        self.update_id = new_updates.getUpdates(100, 999999999999999)
+        # self.__updates.append(self.update_id)
+        self.__updates.append(new_updates)
+        # return self.__updates
         return self.__updates
 
 
@@ -28,13 +53,15 @@ class telegram:
 def main():
 
     print("Starting bot.py")
+    
+    update_id = None
+
     try:
         BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
 
         if isDebugOn:
             print("[DEBUG] KEY: %s" % (os.environ.get('TG_BOT_TOKEN')))
             print("TOKEN: %s" % (BOT_TOKEN))
-
         if not BOT_TOKEN:
             raise Exception
         if isDebugOn:
@@ -49,13 +76,26 @@ def main():
 
     # dynamically setting the bot token
     bot.set_token(BOT_TOKEN)
+ 
+    # for debugging purposes    
+    if isDebugOn:
+        print("[DEBUG:11111]: %s" % type(bot.get_updates()[0].update_id))
+        print("[DEBUG:11111]: %s" % bot.get_updates()[0].update_id)
 
     try:
         update_id = bot.get_updates()[0].update_id
+        if update_id is None:
+            raise Exception
         if isDebugOn:
-            print("[DEBUG] update_id: %d" % (update_id))
+            print("[DEBUG:33333] update_id: %s" % type(update_id))
+            print("[DEBUG:33333] update_id: %s" % update_id)
     except Exception:
+        if isDebugOn:
+            print("[DEBUG][Error] An exception was thrown: invalid update_id!")
+            print("[DEBUG][UPDATE] UpdateMessage: %s" % (update_id))
         update_id = None
+
+
 
 if __name__ == '__main__':
     main()
